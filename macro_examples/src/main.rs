21f0a97acc0a36c8744851af673b1252e9eb0ed8
steadylearner <32325099+steadylearner@users.noreply.github.com>
2020-01-16 09:53:51 -0500

    Include macro examples and should read more doucmenations about macro in Rust.

---
+++ macro_examples/src/main.rs
@@ -0,0 +1,28 @@
+// 1. https://doc.rust-lang.org/rust-by-example/macros.html, https://danielkeep.github.io/practical-intro-to-macros.html
+// 2. Read some blog posts from https://www.google.com/search?&q=rust+macro+example
+// 3. Refer to https://github.com/dtolnay/quote and https://doc.rust-lang.org/proc_macro/
+// 4. Clone and read the code of https://github.com/mjkillough/factori 
+
+use std::collections::HashMap;
+
+macro_rules! map {
+    // => or, :
+    ($( $key:expr => $value:expr ),*) => {{
+        let mut hm = HashMap::new();
+        $( hm.insert($key, $value); )*
+        hm
+    }}
+}
+
+fn main() {
+    let user = map!(
+       "name" => "Steadylearner",
+       "language" => "Rust, Python, JavaScript, (Golang)",
+       "website" => "https://www.steadylearner.com",
+       "blog" => "https://www.steadylearner.com/blog/search/Rust",
+       "portfolio" => "https://github.com/steadylearner/Rust-Full-Stack",
+       "wanting_to_work_with_rust" => "true",
+       "thre_is_no_rust_jobs" => "true at least here"
+    );
+    println!("{:#?}", &user);
+}
